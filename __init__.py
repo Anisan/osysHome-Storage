baba102f2945552fc78ea5e9431541614fa0a4b3
Eraser <eraser1981@gmail.com>
2025-04-07 13:43:32 +0300

    links for subdir current path add icon

--- __init__.py
+++ __init__.py
@@ -1,8 +1,7 @@
 import os
 import datetime
 from settings import Config
-from app.authentication.handlers import handle_admin_required
-from flask import redirect, request, url_for, render_template
+from flask import redirect, render_template
 from app.core.main.BasePlugin import BasePlugin
 
 class Storage(BasePlugin):
@@ -11,13 +10,23 @@ class Storage(BasePlugin):
         super().__init__(app,__name__)
         self.title = "Storage"
         self.description = """File manager for storage"""
-    
+        self.category = "System"
+
     def initialization(self):
         os.makedirs(Config.FILES_DIR, exist_ok=True)
+        os.makedirs(os.path.join(Config.FILES_DIR,"secure"), exist_ok=True)
+        os.makedirs(os.path.join(Config.FILES_DIR,"private"), exist_ok=True)
+        os.makedirs(os.path.join(Config.FILES_DIR,"public"), exist_ok=True)
 
     def admin(self, request):
         op = request.args.get("op", None)
         path = request.args.get("path","")
+
+        path = path.replace("/", os.sep)
+
+        full_path = os.path.join(Config.FILES_DIR, path)
+        if not os.path.commonpath([Config.FILES_DIR, full_path]).startswith(Config.FILES_DIR):
+            return "Access denied", 403
 
         if op == 'create_dir':
             new_dir = request.form['new_dir']
@@ -46,18 +55,33 @@ class Storage(BasePlugin):
             os.remove(full_path)
             return redirect('Storage?path=' + current_path)
         elif op == 'view_file':
-            full_path = os.path.join(Config.FILES_DIR, path)
             with open(full_path, 'r') as f:
                 content = f.read()
             return render_template('file_view.html', content=content)
-        
-        full_path = os.path.join(Config.FILES_DIR, path)
+
         if not os.path.exists(full_path):
             return "Directory not found", 404
-            
+
         items = self.list_files_and_dirs(path)
-        return render_template('storage_main.html', items=items, current_path=path)
-    
+
+        # Generate breadcrumb
+        breadcrumbs = self.__generate_breadcrumbs(path)
+
+        return render_template('storage_main.html', items=items, current_path=path, breadcrumbs=breadcrumbs)
+
+    def __generate_breadcrumbs(self, path):
+        parts = path.split(os.sep)
+        breadcrumbs = []
+        accumulated_path = ""
+        breadcrumbs.append(("", "Home"))  # Добавляем корневой элемент
+
+        for part in parts:
+            if part:  # Пропускаем пустые части
+                accumulated_path = os.path.join(accumulated_path, part).replace("\\", "/")
+                breadcrumbs.append((accumulated_path, part))
+
+        return breadcrumbs
+
     def list_files_and_dirs(self, path):
         IMAGE_EXTENSIONS = {"jpg", "jpeg", "png", "bmp", "gif", "tiff", "webp"}
         items = []
